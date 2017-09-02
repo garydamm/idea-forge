86f710159ff09dbe91b8c912dc2dc2c5fca02b3f
Gary Damm <garydamm@gmail.com>
2017-09-02 15:28:11 -0700

    initial code commit

---
+++ src/main/java/org/damm/ideaforge/IdeaForgeApplication.java
@@ -0,0 +1,12 @@
+package org.damm.ideaforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaForgeApplication.class, args);
+	}
+}
